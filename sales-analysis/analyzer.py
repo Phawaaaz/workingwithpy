308ddaa21a296f6ed9b0e8fe2df2639a7bf6ffff
phawaaaz <phawaazakin@gmail.com>
2026-03-30 15:39:48 +0100

    Updating the code

--- sales-analysis/analyzer.py
+++ sales-analysis/analyzer.py
@@ -14,17 +14,31 @@ if os.path.exists(data_folder):
 else:    
     print("Data file does not exist. Please create the file and add the sales data.")
 
+sales_data = pd.read_csv("data/sales.csv", sep='\t', on_bad_lines="skip")
 
 
-data_folder['total'] = data_folder['quantity'] * data_folder['unit_price']
+
+sales_data['total'] = sales_data['Quantity'] * sales_data['Unit_Price']
+print("\nWith Total:")
+print(sales_data.head())
+
+os.makedirs('output', exist_ok=True)
+sales_data.to_csv('output/sales_with_total.csv', index=False)
+
+# Save as different formats
+# 1. Excel
+sales_data.to_excel('output/sales_calculated.xlsx', index=False)
+
+# 2. JSON
+sales_data.to_json('output/sales_calculated.json', orient='records', lines=True)
+
 
 
 # Read the sales data from the CSV file
-sales_data = pd.read_csv("data/sales.csv", sep='\t', on_bad_lines="skip")
 print("Sales data loaded successfully.")
 print(sales_data.head())
 # Analyze the sales data
-total_sales = sales_data['Revenue'].sum()
+total_sales = sales_data['Revenue'].sum()0
 average_sales = sales_data['Revenue'].mean()
 print(f"Total Sales: ${total_sales:.2f}")
 print(f"Average Sales: ${average_sales:.2f}")
